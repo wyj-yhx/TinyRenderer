abef221d4cd56c12c84577db0b63f9d4ee47c0c8
wyj-yhx <1638580922@qq.com>
2025-08-26 18:33:59 +0800

    添加注释

--- TinyRenderer/main.cpp
+++ TinyRenderer/main.cpp
@@ -1,9 +1,13 @@
 #include "tgaimage.h"
 
+#include <iostream>
+using namespace std;
+
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 
 int main(int argc, char** argv) {
+	
 	TGAImage image(100, 100, TGAImage::RGB);
 	image.set(52, 41, red);
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
