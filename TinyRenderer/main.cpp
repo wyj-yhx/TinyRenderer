b7832eae87c3fc4404e3916ae9b381c9617388d6
wyj-yhx <1638580922@qq.com>
2025-09-17 15:22:30 +0800

    添加模型的颜色

--- TinyRenderer/main.cpp
+++ TinyRenderer/main.cpp
@@ -70,6 +70,9 @@ struct PhongShader : IShader {
 	/*****法线贴图******/
 	vec4 l;              // light direction in eye coordinates
 	vec2 varying_uv[3];  // triangle uv coordinates, written by the vertex shader, read
+	/******切线空间法线贴图******/
+	vec4 varying_nrm[3]; // normal per vertex to be interpolated by the fragment shader
+	vec4 tri[3];         // triangle in view coordinates
 
 	PhongShader(const vec3 light, const Model& m) : model(m) {
 		//l = normalized((ModelView * vec4{ light.x, light.y, light.z, 0. }).xyz()); // transform the light vector to view coordinates 将光向量转换为视图坐标
@@ -86,13 +89,18 @@ struct PhongShader : IShader {
 		//tri[vert] = gl_Position.xyz();                            // in eye coordinates
 		/*****法线贴图******/
 		varying_uv[vert] = model.uv(face, vert);
+		/*****切线空间法线贴图******/
+		varying_nrm[vert] = ModelView.invert_transpose() * model.normal(face, vert);
 		vec4 gl_Position = ModelView * model.vert(face, vert);
 		gl_Position.y = -gl_Position.y;
+
+		/*****切线空间法线贴图******/
+		tri[vert] = gl_Position;
 		return Perspective * gl_Position;                         // in clip coordinates
 	}
 
 	virtual std::pair<bool, TGAColor> fragment(const vec3 bar) const {
-		TGAColor gl_FragColor = { 255, 255, 255, 255 };             // output color of the fragment
+		//TGAColor gl_FragColor = { 255, 255, 255, 255 };             // output color of the fragment
 		//vec3 n = normalized(cross(tri[2] - tri[0], tri[1] - tri[0]));// per-vertex normal 添加光的反射
 		/******平滑处理********/
 		//vec3 n = normalized(varying_nrm[0] * bar[0] + varying_nrm[1] * bar[1] + varying_nrm[2] * bar[2]);// per-vertex normal 
@@ -102,17 +110,41 @@ struct PhongShader : IShader {
 		/*****法线贴图******/ 
 		//vec2 uv = varying_uv[0] * bar[0] + varying_uv[1] * bar[1] + varying_uv[2] * bar[2];
 		// 将2，0反转，
-		vec2 uv = varying_uv[2] * bar[0] + varying_uv[1] * bar[1] + varying_uv[0] * bar[2];
-		vec4 n = normalized(ModelView.invert_transpose() * model.normal(uv));
+		/*****切线空间法线贴图******/
+		mat<2, 4> E = { tri[1] - tri[0], tri[2] - tri[0] };
+		mat<2, 2> U = { varying_uv[1] - varying_uv[0], varying_uv[2] - varying_uv[0] };
+		mat<2, 4> T = U.invert() * E;
+		mat<4, 4> D = { normalized(T[0]),  // tangent vector
+					  normalized(T[1]),  // bitangent vector
+					  normalized(varying_nrm[0] * bar[2] + varying_nrm[1] * bar[1] + varying_nrm[2] * bar[0]), // interpolated normal
+					  {0,0,0,1} }; // Darboux frame
+
+		vec2 uv = varying_uv[0] * bar[2] + varying_uv[1] * bar[1] + varying_uv[2] * bar[0];
+
+		//vec4 n = normalized(ModelView.invert_transpose() * model.normal(uv));
+
+		/*****切线空间法线贴图******/
+		vec4 n = normalized(D.transpose() * model.normal(uv));
 		vec4 r = normalized(n * (n * l) * 2 - l);                   // reflected light direction
 
-		double ambient = .3;                                      // ambient light intensity
-		double diff = std::max(0., n * l);                        // diffuse light intensity
-		double spec = std::pow(std::max(r.z, 0.), 35);            // specular intensity, note that the camera lies on the z-axis (in eye coordinates), therefore simple r.z, since (0,0,1)*(r.x, r.y, r.z) = r.z
-		for (int channel : {0, 1, 2}){
-			gl_FragColor[channel] *= std::min(1., ambient + .4 * diff + .9 * spec);
-			//cout << ambient << " | " << diff << " | " << l << " | " << endl;
-		}
+		double ambient = .4;                                      // ambient light intensity
+		/*******法线*******/
+		//double diff = std::max(0., n * l);                        // diffuse light intensity
+		//double spec = std::pow(std::max(r.z, 0.), 35);            // specular intensity, note that the camera lies on the z-axis (in eye coordinates), therefore simple r.z, since (0,0,1)*(r.x, r.y, r.z) = r.z
+		//for (int channel : {0, 1, 2}){
+		//	gl_FragColor[channel] *= std::min(1., ambient + .4 * diff + .9 * spec);
+		//	//cout << ambient << " | " << diff << " | " << l << " | " << endl;
+		//}
+
+		/*****贴图******/
+		double diffuse = 1. * std::max(0., n * l);                 // diffuse light intensity
+		double specular = (1. + 3. * sample2D(model.specular(), uv)[0] / 255.) * std::pow(std::max(r.z, 0.), 35);  // specular intensity, note that the camera lies on the z-axis (in eye coordinates), therefore simple r.z, since (0,0,1)*(r.x, r.y, r.z) = r.z
+		TGAColor gl_FragColor = sample2D(model.diffuse(), uv);
+		//TGAColor gl_FragColor = {255, 255, 255, 255};
+		for (int channel : {0, 1, 2})
+			gl_FragColor[channel] = std::min<int>(255, gl_FragColor[channel] * (ambient + diffuse + specular));
+
+
 		return { false, gl_FragColor };                             // do not discard the pixel
 	}
 };
